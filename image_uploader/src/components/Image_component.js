f2f64815ee709c00b76fdf49c0e05b60b6b5af41
Melvin9 <melvinmathew0102@gmail.com>
2020-08-04 14:43:10 +0530

    Ui Updated

--- image_uploader/src/components/Image_component.js
+++ image_uploader/src/components/Image_component.js
@@ -1,20 +1,22 @@
 import React from "react";
 
 const Image_component = () => {
-
-    return(
+  return (
     <div>
-        <form style={{marginTop: "15%"  }}>
-            <h1 className="ui center aligned icon header" style={{ color: "white" }}>Image Recognizer</h1>
-            <h2 className="ui center aligned icon header" style={{ color: "white" }}>Drop Your Images Here</h2>
-            <div className="ui center aligned basic segment">
-                <div className="ui big icon input" style={{ padding: "10px", border: "3px solid white" }}>
-                    <input type="file" />
-                    <button className="ui button">Upload</button>
-                </div>
-            </div>   
-        </form>
+      <form className="ui form">
+        <h1 className="ui dividing header">Image Recognizer</h1>
+        <div className="field">
+          <div class="ui raised segment">
+            <a class="ui teal ribbon label">Drop Your Images Here</a>
+            <div className="ui action input">
+              <br></br>
+              <input type="file" />
+              <button className="ui green button">Upload</button>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
-    );
+  );
 };
 export default Image_component;
--- image_uploader/src/index.js
+++ image_uploader/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Image_component from './components/Image_component';
 
 const App = ()=>{
-  return(<div style={{background: "#003333"}}> <Image_component></Image_component></div>);
+  return(<div className = "ui container"> <Image_component></Image_component></div>);
 };
 ReactDOM.render(
   <App />,
