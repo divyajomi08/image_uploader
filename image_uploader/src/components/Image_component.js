503ef7417f80641af784470da019f2a85dbe27f7
Melvin9 <melvinmathew0102@gmail.com>
2020-08-05 16:02:46 +0530

    bugs Fixed

--- image_uploader/src/components/Image_component.js
+++ image_uploader/src/components/Image_component.js
@@ -1,43 +1,41 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const Image_component = () => {
-  const [selectedfile,setfile]=useState(null);
-  const [identifier,setid]=useState(null);
+  const [selectedFile, setFile] = useState(null);
+  const [identifier, setId] = useState(null);
+  const [uploaded, setUpload] = useState(null);
 
-  const onFileChange = event => {
-    console.log(event.target.files[0])
-    setfile(event.target.files[0]);
+  const onFileChange = (event) => {
+    console.log(event.target.files[0]);
+    setFile(event.target.files[0]);
   };
 
-  const onTextChange = event =>{
-    setid(event.target.value);
+  const onTextChange = (event) => {
+    setId(event.target.value);
   };
 
   const onFileUpload = (event) => {
-    console.log(selectedfile);
+    console.log(selectedFile);
     event.preventDefault();
     const formData = new FormData();
-    formData.append(
-      "image",
-      selectedfile
-    );
-    formData.append(
-      "identifier",
-      identifier
-    );
-    axios.post("https://stormy-dawn-26142.herokuapp.com/images", formData, {
-      headers: {
-        'Content-Type': "multipart/formdata",
-      }
-    }).catch((err)=>{
-      console.log(err.response);
-    });
-   
+    formData.append("image", selectedFile);
+    formData.append("identifier", identifier);
+    axios
+      .post(" https://stormy-dawn-26142.herokuapp.com/images", formData, {
+        headers: {
+          "Content-Type": "multipart/formdata",
+        },
+      })
+      .then((res) => setUpload(true))
+      .catch((err) => {
+        console.log(err.response);
+        setUpload(null);
+      });
   };
   return (
     <div>
-      <form className = "ui form" >
+      <form className="ui form">
         <h1 className="ui dividing header">Image Recognizer</h1>
         <div className="field">
           <div className="ui raised segment">
@@ -48,9 +46,22 @@ const Image_component = () => {
             </div>
             <div className="field">
               <label>Name</label>
-              <input name="identifier" onChange={onTextChange} type="text" placeholder="name" />
+              <input
+                name="identifier"
+                onChange={onTextChange}
+                type="text"
+                placeholder="name"
+              />
             </div>
-            <button className="ui green button" onClick={onFileUpload} >Upload</button>
+            <button className="ui green button" onClick={onFileUpload}>
+              Upload
+            </button>
+            <div>
+              <br></br>
+              {uploaded ? 
+                 <h4 class="ui dividing header">Uploaded</h4>
+               : null}
+            </div>
           </div>
         </div>
       </form>
